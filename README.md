1fa91f8b4c1293dede440a4f083a64ad7a55ba04
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-04-03 16:38:35 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,39 +1,14 @@
 # Test Challenge 1
 
-You are given a Calculator API that exposes the following API:
+1. Create a calculator that has the exposes the following method:
 
-- add()
+- plus()
 - minus()
-- times()
-- product()
+- multiply()
+- divide()
+- undo()
+- reset()
 
-This is an example on how the add operation works:
-
-```javascript
-const calculator = new Calculator()
-
-console.log(calculator.value) // Logs 0
-
-const output1 = calculator.add(10) // Returns 10
-console.log(calculator.value) // Logs 0
-
-calculator.save(output1)
-console.log(calculator.value) // Logs 10
-```
-
-You can also add multiple values:
-
-```javascript
-const calculator = new Calculator()
-
-const output1 = calculator.add(10, 20, 30)
-console.log(output1) // Logs 60
-console.log(calculator.value) // Logs 0
-
-calculator.save(output1)
-console.log(calculator.value) // Logs 60
-```
-
-The same applies to the operators `minus`, `times`, and `product`.
-
-Write a unit test to ensure the calculator is working fine. You can also refactor the code to ensure it is working correctly. 
+2. Ensure that each operation is working correctly.
+3. The methods shall be chainable
+4. Write a unit test to ensure the calculator is working fine.
--- calculator.js
+++ calculator.js
@@ -1,67 +1,52 @@
 class Calculator {
   constructor () {
-    // Holds the current value
-  	this.value = 0
-
-    // Holds the last value
-    this.lastValue = 0
-
     // Holds the list of values
     this.values = [0]
-
-    // Holds the last operator
-    this.lastOperator = null
 
     // Holds the list of operators
     this.operators = []
   }
-  add () {
-  	const values = [...arguments]
-    this.lastOperator = 'add'
-    this.lastValue = values.reduce((a, b) => a + b, this.value)
-  	return this.lastValue
+  plus () {
+    const values = [...arguments]
+    const output = values.reduce((a, b) => a + b, this.value)
+    this.operators.push('add')
+    this.values.push(output)
+    return this
   }
   minus () {
     const values = [...arguments]
-    this.lastOperator = 'minus'
-    this.lastValue = values.reduce((a, b) => a - b, this.value)
-    return this.lastValue
+    const output = values.reduce((a, b) => a - b, this.value)
+    this.operators.push('minus')
+    this.values.push(output)
+    return this
   }
-  times () {
+  multiply () {
     const values = [...arguments]
-    this.lastOperator = 'times'
-    this.lastValue = values.reduce((a, b) => a * b, this.value)
-    return this.lastValue
+    const output = values.reduce((a, b) => a * b, this.value)
+    this.operators.push('times')
+    this.values.push(output)
+    return this
   }
   divide () {
     const values = [...arguments]
-    this.lastOperator = 'divide'
-    this.lastValue = values.reduce((a, b) => a / b, this.value)
-    return this.lastValue
+    const output = values.reduce((a, b) => a / b, this.value)
+    this.operators.push('divide')
+    this.values.push(output)
+    return this
   }
-  save (val) {
-  	this.value = val
 
-    this.values.push(this.value)
-    this.operators.push(this.lastOperator)
-
-    this.lastValue = 0
-    this.lastOperator = null
+  get value () {
+    return this.values[this.values.length - 1]
   }
   reset () {
-  	this.value = 0
-    this.lastValue = 0
-    this.lastOperator = null
     this.values = [0]
     this.operators = []
+    return this
   }
   undo () {
     this.operators.pop()
     this.values.pop()
-
-    this.lastOperator = this.operators[this.operators.length - 1]
-    this.lastValue = this.values[this.values.length - 1]
-    this.value = this.lastValue
+    return this
   }
 }
 
